e37f0de2971fb616479554b43df3ac5ec8392fba
eliataylor <eli@taylormadetraffic.com>
2021-10-08 01:05:17 -0500

    dmeo

--- data/StaticData.js
+++ data/StaticData.js
@@ -1,10 +1,13 @@
 import test1 from './test1.json';
+import test2 from './test2.json';
+import test3 from './test3.json';
+import test4 from './test4.json';
 
 const allLists = {
     '/airport/list': {label: 'Airports', json:test1},
-    '/waypoint/list': {label: 'Waypoints', json:test1},
-    '/procedure/list': {label: 'Procedures', json:test1},
-    '/navaid/list': {label: 'NavAid', json:test1}
+    '/waypoint/list': {label: 'Waypoints', json:test2},
+    '/procedure/list': {label: 'Procedures', json:test3},
+    '/navaid/list': {label: 'NavAid', json:test4}
 };
 
 export default function StaticData(l) {
--- data/data-builder.js
+++ data/data-builder.js
@@ -2,8 +2,11 @@ const fs = require('fs');
 
 let arr = [];
 for(let i=0; i < 20000; i++) {
-    arr.push({label:'test' + 1, value:i});
+    arr.push({"Name":'test' + i, "Value":i});
 }
 fs.writeFileSync('./data/test1.json', JSON.stringify(arr), 'utf8');
+fs.writeFileSync('./data/test2.json', JSON.stringify(arr), 'utf8');
+fs.writeFileSync('./data/test3.json', JSON.stringify(arr), 'utf8');
+fs.writeFileSync('./data/test4.json', JSON.stringify(arr), 'utf8');
 
 console.log('Wrote large json files');
